d6b6e60bc2a1681af5c617cbae75eb53f4e47d5a
LawNguyenIT <lawnguyenit@gmail.com>
2025-03-16 00:42:10 +0700

    Điều chỉnh caulifa

--- src/red/team/boss/list_boss/caulifa/Caulifa.java
+++ src/red/team/boss/list_boss/caulifa/Caulifa.java
@@ -22,32 +22,35 @@ public class Caulifa extends Boss {
     @Override
     public void reward(Player plKill) {
         byte randomDo = (byte) new Random().nextInt(Manager.itemIds_TL.length - 1);
-        byte randomNR = (byte) new Random().nextInt(Manager.itemIds_NR_SB.length);
-        int[] itemDos = new int[] { 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567 };
-        int randomc12 = new Random().nextInt(itemDos.length);
-        if (Util.isTrue(BossManager.ratioReward, 100)) {
-            if (Util.isTrue(1, 5)) {
-                Service.gI().dropItemMap(this.zone,
-                        Util.ratiItem(zone, 561, 1, this.location.x, this.location.y, plKill.id));
+        byte randomNR = (byte) new Random().nextInt(Manager.itemIds_NR_SB.length -1);
 
+        if (Util.isTrue(15, 20)) {
+//                int[] manhthuong = new int[] { 1066, 1067, 1068, 1069, 1070 };  
+            int[] manhhiem = new int[]{561};
+            int[] gang = new int[]{562, 564, 566};
+            int randomG = new Random().nextInt(gang.length - 1);
+//                int randomAWJ = new Random().nextInt(manhthuong.length-1);
+            int randomGR = new Random().nextInt(manhhiem.length - 1);
+            if (Util.isTrue(70, 100)) {
+//                    Service.gI().dropItemMap(this.zone,
+//                            Util.manhTS(zone, manhthuong[randomAWJ], 1, this.location.x, this.location.y, plKill.id));
+                Service.gI().dropItemMap(this.zone,
+                        Util.ratiItem(zone, (byte) 15, 1, this.location.x + 2, this.location.y, plKill.id));
+            } else {
+                Service.gI().dropItemMap(this.zone,
+                        Util.manhTS(zone, manhhiem[randomGR], 3, this.location.x, this.location.y, plKill.id));
             }
             Service.gI().dropItemMap(this.zone,
-                    Util.ratiItem(zone, Manager.itemIds_TL[randomDo], 1, this.location.x, this.location.y, plKill.id));
-        } else if (Util.isTrue(2, 5)) {
-            Service.gI().dropItemMap(this.zone,
-                    Util.RaitiDoc12(zone, itemDos[randomc12], 1, this.location.x, this.location.y, plKill.id));
-
+                    new ItemMap(zone, 992, 1, this.location.x, this.location.y, plKill.id));
+            if (Util.isTrue(5, 100)) {
+                Service.gI().dropItemMap(this.zone,
+                        Util.ratiItem(zone, gang[randomG], 1, this.location.x, this.location.y, plKill.id));
+            }
         } else {
-            Service.gI().dropItemMap(this.zone,
-                    new ItemMap(zone, Manager.itemIds_NR_SB[randomNR], 1, this.location.x, this.location.y, plKill.id));
+            Service.gI().dropItemMap(this.zone, Util.ratiItem(zone, Manager.itemIds_TL[randomDo], 1,
+                    this.location.x, this.location.y, plKill.id));
         }
-        for (Integer i = 0; i < 3; i++) {
-            Service.gI().dropItemMap(this.zone,
-                    Util.ratiItem(zone, 344, 1, this.location.x + 2, this.location.y, plKill.id));
-            Service.gI().dropItemMap(this.zone,
-                    Util.ratiItem(zone, 2000 + plKill.gender, 1, this.location.x + 10 * i, this.location.y, -1));
-        }
-        plKill.pointBoss += 0;
+        plKill.pointBoss += 2;
         TaskService.gI().checkDoneTaskKillBoss(plKill, this);
     }
 
