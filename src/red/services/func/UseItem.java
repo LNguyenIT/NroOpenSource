eec9ad59dc26229e650212563d111fa5b222bd3c
LawNguyenIT <lawnguyenit@gmail.com>
2025-03-15 14:27:34 +0700

    Yêu cầu khi thay đệ

--- src/red/services/func/UseItem.java
+++ src/red/services/func/UseItem.java
@@ -1398,9 +1398,9 @@ public class UseItem {
 
             
             int tv = Util.nextInt(gold[0][0], gold[0][1]);
-
-             int itemc2 = new Random().nextInt(Manager.itc2.length - 1);
-            if (Util.isTrue(1  , 100)) {
+            int itemc2 = new Random().nextInt(0, Manager.itc2.length - 1);
+            
+            if (Util.isTrue(10  , 100)) {
                 Item it = ItemService.gI().createNewItem((short) (457), tv);
                 InventoryServiceNew.gI().addItemBag(pl, it);
 
@@ -1416,14 +1416,11 @@ public class UseItem {
 
                 Item it = ItemService.gI().createNewItem((short)Manager.itc2[itemc2], 1);
                 InventoryServiceNew.gI().addItemBag(pl, it);
-
                 Service.gI().sendThongBao(pl, "Bạn vừa mở ra item cấp 2!");
             } else {
                 Item it = ItemService.gI().createNewItem(temp[index],1);
 //                Item it = ItemService.gI().createNewItem((short)Manager.itc2[itemc2], 1);
                 InventoryServiceNew.gI().addItemBag(pl, it);
-                Service.gI().sendThongBao(pl, "Bạn vừa mở ra item cấp 1!");
-
             }
             InventoryServiceNew.gI().subQuantityItemsBag(pl, item, 1);
             InventoryServiceNew.gI().sendItemBags(pl);
