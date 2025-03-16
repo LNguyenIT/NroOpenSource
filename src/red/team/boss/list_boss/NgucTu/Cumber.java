c5abc26cd164bc712f9612a6f3104856d93ed875
LawNguyenIT <lawnguyenit@gmail.com>
2025-03-16 14:10:31 +0700

    Sửa kiểu dữ liệu

--- src/red/team/boss/list_boss/NgucTu/Cumber.java
+++ src/red/team/boss/list_boss/NgucTu/Cumber.java
@@ -40,10 +40,10 @@ public class Cumber extends Boss {
                 Service.gI().dropItemMap(this.zone,
                         Util.ratiItem(zone, 561, 1, this.location.x, this.location.y, plKill.id));
             }
-            if (Util.isTrue(1, 10)) {
-                Service.gI().dropItemMap(this.zone,
-                        Util.ratiItem(zone, 2000 + plKill.gender, 1, this.location.x, this.location.y, plKill.id));
-            }
+//            if (Util.isTrue(1, 10)) {
+//                Service.gI().dropItemMap(this.zone,
+//                        Util.ratiItem(zone, 2000 + plKill.gender, 1, this.location.x, this.location.y, plKill.id));
+//            }
             if (Util.isTrue(1, 3)) {
                 Service.gI().dropItemMap(this.zone,
                         Util.ratiItem(zone, 344, 1, this.location.x + 2, this.location.y, plKill.id));
