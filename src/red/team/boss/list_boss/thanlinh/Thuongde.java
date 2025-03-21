a4d223b78000d2bd968116bdc4039cb29262bafc
LawNguyenIT <lawnguyenit@gmail.com>
2025-03-21 19:47:57 +0700

    Cập nhật việc thiếu đồ do int?

--- src/red/team/boss/list_boss/thanlinh/Thuongde.java
+++ src/red/team/boss/list_boss/thanlinh/Thuongde.java
@@ -27,10 +27,10 @@ public class Thuongde extends Boss {
         int randomDo = new Random().nextInt(itemDos.length);
         if (Util.isTrue(30, 100)) {
             
-                if (Util.isTrue(6, 10)) {
-                    Service.gI().dropItemMap(this.zone, Util.ratiItem(zone, 2000+plKill.gender, 1, this.location.x, this.location.y, plKill.id));
-                }
-            
+//                if (Util.isTrue(6, 10)) {
+//                    Service.gI().dropItemMap(this.zone, Util.ratiItem(zone, 2000+plKill.gender, 1, this.location.x, this.location.y, plKill.id));
+//                }
+//            
             if (Util.isTrue(1, 3)) {
                 Service.gI().dropItemMap(this.zone, Util.ratiItem(zone, 344, 1, this.location.x+2, this.location.y, plKill.id));
             }
